1268b7b0f6237f7fe0526b2c60bebf3cfcc97b79
chrslgn <chrs.lgn@gmail.com>
2013-02-06 01:17:46 -0500

    Added a locale, currently hard coded to US.

--- TransitIon/src/com/example/transition/settings/AppConfig.java
+++ TransitIon/src/com/example/transition/settings/AppConfig.java
@@ -1,9 +1,12 @@
 package com.example.transition.settings;
 
+import java.util.Locale;
+
 import com.example.transition.lib.enums.LogLevel;
 
 /**
  * This class holds all of the configurations and settings of the app during runtime.
+ * @since 06/02/2013
  * @author Chris Logan
  *
  */
@@ -15,6 +18,11 @@ public class AppConfig {
 	private static LogLevel logLevel = LogLevel.DEBUG;
 	
 	/**
+	 * Default locale is US. This will store the locale set by the user.
+	 */
+	private static Locale locale = Locale.US; 
+	
+	/**
 	 * Determines if the {@link LogLevel} provided is allowed to be logged.
 	 * @param level The level attempting to be logged.
 	 * @return True if the level is at or above the current log level. False if this level of logging is not allowed.
@@ -23,5 +31,13 @@ public class AppConfig {
 		return logLevel.getLevel() >= level.getLevel();
 	}
 
+	/**
+	 * Getter for the locale that is currently set.
+	 * @return The current locale.
+	 */
+	public static Locale getCurrentLocale()
+	{
+		return locale;
+	}
 
 }
